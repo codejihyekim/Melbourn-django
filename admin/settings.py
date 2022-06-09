2e69b414b76527795c077f4c9fb4213a4256a8da
codejihyekim <wlgp03090@gmail.com>
2022-06-09 11:11:42 +0900

    2022-06-09 db연결 전

--- admin/settings.py
+++ admin/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
+        'NAME': 'mibotdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3307'
     }
 }
 
