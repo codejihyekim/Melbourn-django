4702f507bac43582b78570ab44a5ab46c493af48
codejihyekim <wlgp03090@gmail.com>
2022-06-28 19:53:22 +0900

    tensorflow 2 생성

---
+++ movie/movie.py
@@ -0,0 +1,56 @@
+import re
+import urllib.request
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+
+from konlpy.tag import Mecab
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def preprocessing(self):
+        train_file = urllib.request.urlopen("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt")
+        test_file = urllib.request.urlopen("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt")
+        train_data = pd.read_table(train_file)
+        test_data = pd.read_table(test_file)
+        #print(train_data[:10])
+
+        #print(train_data['document'].nunique()) #중복확인
+        #print(train_data['label'].nunique()) #중복확인
+        train_data.drop_duplicates(subset=['document'], inplace=True) # document의 중복을 제거
+        #print(train_data.isnull().sum()) # null의 개수를 출력
+        train_data = train_data.dropna(how='any') # null값을 제거
+
+        train_data['document'] = train_data['document'].str.replace("[^ㄱ-ㅣ가-힣 ]", "") #한글 아닌 것은 제거
+        #print(train_data[:10])
+
+        train_data['document'].replace('', np.nan, inplace=True) # 전처리
+        #print(len(train_data))
+        #print(train_data.isnull().sum())
+
+        train_data = train_data.dropna(how='any') # 전처리 후 생기는 null값 제거
+        #print(len(train_data))
+
+        #test_data 전처리
+        test_data.drop_duplicates(subset=['document'], inplace=True)
+        test_data['document'] = test_data['document'].str.replace("[^ㄱ-ㅣ가-힣 ]", "")
+        test_data['document'].replace('', np.nan, inplace=True)
+        test_data = test_data.dropna(how='any')
+
+        stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', ' 과', '도', '를', '으로', '자', '에', '와', '한', '하다']
+        mecab = Mecab()
+        X_train = []
+        for sentence in train_data['document']:
+            X_train.append([word for word in mecab.morphs(sentence) if not word in stopwords]) # 토큰작업와 불용어 처리
+        print(X_train[:1])
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.preprocessing()
--- tf2/hand_writing.py
+++ tf2/hand_writing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from keras import datasets
-from tensorflow import keras # 이미지 처리 ㅍ
+from keras import datasets, layers, models
+from tensorflow import keras # 이미지 처리
 
 
 class Solution:
@@ -17,13 +17,34 @@ class Solution:
 
         # 픽셀 값을 0~1 사이로 정규화합니다.
         self.train_images, self.test_images = train_images / 255.0, test_images / 255.0
+        self.model = models.Sequential()
+        self.train_labels = train_labels
+        self.layers = layers
 
     def solution(self):
-        models = keras.models
-        layers = keras.layers
-        model = models.Sequential()
+        self.modeling()
+        self.training()
+
+    def modeling(self):
+        model = self.model
+        layers = self.layers
         model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(10, activation='softmax'))
+        model.summary()
+
+    def training(self):
+        model = self.model
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.fit(self.train_images, self.train_labels, epochs=5)
+
+if __name__ == '__main__':
+    Solution().solution()
